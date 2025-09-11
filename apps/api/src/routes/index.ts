a647274fbaed7006c1e1f0f6df5a28f27cd00e3c
Rashmiranjan_Dash <guddudash73@gmail.com>
2025-09-11 20:58:24 +0530

    fixed some bug

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from "./users/users.router";
 import authRouter from "../auth/auth.router";
 
 const router = Router();
+
 router.use("/", health);
 router.use("/users", usersRouter);
 router.use("/auth", authRouter);
